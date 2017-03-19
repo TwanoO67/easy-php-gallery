3a0971aa38b7f102b534a8c8d40b7c542d2ee1f9
WEBER Antoine <ghostsmaker@hotmail.com>
2017-03-19 09:30:36 +0100

    maj du script de deploiement

--- config/php/src/init.sh
+++ config/php/src/init.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+echo "Ouverture des droits"
+chmod -R 777 storage
+chmod -R 777 database.sqlite
+chmod -R 777 bootstrap/cache
 
 echo "Installation des dépendeances"
 composer install
+
+if [ ! -f .env ]; then
+    echo "Création du fichier d'env de laravel"
+    cp .env.example .env
+fi
 
 echo "Génération d'une clef de sécurité"
 php artisan key:generate
 
 if [ ! -f /var/www/html/database.sqlite ]; then
     echo "Création de la base de donnée"
-    touch /var/www/html/database.sqlite
+    touch database.sqlite
+    chmod -R 777 database.sqlite
     php artisan migrate
     php artisan db:seed
 fi
