cf8acd4eb3ab1443765829dca64df6978c898fda
WEBER Antoine <ghostsmaker@hotmail.com>
2017-03-19 19:00:25 +0100

    modif de css jquery ui

--- config/php/src/resources/views/admin.blade.php
+++ config/php/src/resources/views/admin.blade.php
@@ -79,7 +79,7 @@
   src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
   integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
   crossorigin="anonymous"></script>
-  <link rel="stylesheet" type="text/css" href=https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
+  <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
   <script>
   $(function()
   {
