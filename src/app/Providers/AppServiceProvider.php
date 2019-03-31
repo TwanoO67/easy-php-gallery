679b41abc7e25ede453d29f260c0e07e88cb454c
WEBER Antoine <ghostsmaker@hotmail.com>
2019-03-31 18:25:57 +0200

    gestion de l'url dans APP_URL

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -24,5 +24,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        \URL::forceRootUrl(\Config::get('app.url'));    
+        // And this if you wanna handle https URL scheme
+        // It's not usefull for http://www.example.com, it's just to make it more independant from the constant value
+        if (str_contains(\Config::get('app.url'), 'https://')) {
+            \URL::forceScheme('https');
+            //use \URL:forceSchema('https') if you use laravel < 5.4
+        }
     }
 }
